1b2670b5a49725840ab4f9d17afe4c3d949f811b
sauli kotisaari <samuk@gmail.com>
2015-06-30 12:52:06 +0300

    CDI support

---
+++ database-orm/src/main/generated/com/samuk/orm/DbSprint_.java
@@ -0,0 +1,13 @@
+package com.samuk.orm;
+
+import java.sql.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="Dali", date="2015-06-30T11:11:55.886+0300")
+@StaticMetamodel(DbSprint.class)
+public class DbSprint_ {
+	public static volatile SingularAttribute<DbSprint, Long> id;
+	public static volatile SingularAttribute<DbSprint, Date> date;
+}
--- database-orm/src/main/java/com/samuk/orm/DbSprint.java
+++ database-orm/src/main/java/com/samuk/orm/DbSprint.java
@@ -1,5 +1,48 @@
 package com.samuk.orm;
 
+import java.sql.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class DbSprint {
 
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	private Date date;
+	
+//	private DbTeam team;
+	
+	public DbSprint(){}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+//	public DbTeam getTeam() {
+//		return team;
+//	}
+//
+//	public void setTeam(DbTeam team) {
+//		this.team = team;
+//	}
+
+	@Override
+	public String toString() {
+		return "DbSprint [id=" + id + ", date=" + date + ", team]";
+	}
+	
+	
+	
+	
+	
 }
