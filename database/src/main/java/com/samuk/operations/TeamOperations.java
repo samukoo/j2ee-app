84393b4d718fb05822443dab02b6e2d762f8bb28
sauli kotisaari <samuk@gmail.com>
2015-06-30 12:52:59 +0300

    CDI support

--- database/src/main/java/com/samuk/operations/TeamOperations.java
+++ database/src/main/java/com/samuk/operations/TeamOperations.java
@@ -1,70 +1,29 @@
 package com.samuk.operations;
 
-import java.util.List;
-
 import javax.ejb.Stateless;
+import javax.enterprise.inject.Default;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import com.samuk.orm.DbTeam;
-import com.samuk.service.TeamService;
 
 /**
- * Session Bean implementation class TeamOperations
+ * TeamOperations Data Access Object
+ * @author kotissa
+ *
  */
+
+@Default
 @Stateless
-public class TeamOperations implements TeamService {
+public class TeamOperations{
 
 	@PersistenceContext
 	private EntityManager em;
 	
-    /**
-     * Default constructor. 
-     */
-    public TeamOperations() {
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-     * @see TeamService#deleteTeam(DbTeam)
-     */
-    public void deleteTeam(DbTeam team) {
-        // TODO Auto-generated method stub
-    }
-
-	/**
-     * @see TeamService#getAllTeams()
-     */
-    public List<DbTeam> getAllTeams() {
-        // TODO Auto-generated method stub
-			return null;
-    }
-
-	/**
-     * @see TeamService#findTeamById(String)
-     */
-    public DbTeam findTeamById(String teamId) {
-        // TODO Auto-generated method stub
-			return null;
-    }
-
-	/**
-     * @see TeamService#updateTeam(DbTeam, DbTeam)
-     */
-    public DbTeam updateTeam(DbTeam newTeam, DbTeam targetTeam) {
-        // TODO Auto-generated method stub
-			return null;
-    }
-
-	/**
-     * @see TeamService#addTeam()
-     */
-    public DbTeam addTeam() {
-        // TODO Auto-generated method stub
-			return null;
-    }
-
-	@Override
+	public void persistTeam(DbTeam team){
+		em.persist(team);
+	}
+	
 	public void printTeam(DbTeam team) {
 		System.out.println(team.toString());
 	}
